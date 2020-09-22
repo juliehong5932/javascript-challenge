1ce1d2a8d343165ace1b946470735bd63b5f24b6
juliehong5932 <juliehong5932@gmail.com>
2020-09-22 02:10:41 -0600

    app file update

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,30 @@
 // from data.js
 var tableData = data;
+console.log(tableData);
 
 // YOUR CODE HERE!
+
+
+//Code that appends a table to your web page and then adds new rows of data for each UFO sighting.
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(ufoData){
+    console.log(ufoData);
+    var row = tbody.append("tr");
+
+Object.entries(ufoData).forEach(function([key, value]) {
+    console.log(key, value);
+    var cell = row.append("td");
+    cell.text(value);
+
+    });
+
+});
+
+var button = d3.select("#filter-btn#");
+
+var inputField = d3.select("#datetime");
+
+var inputValue = inputField.property('value');
+
+button.on("click", handleClick);
